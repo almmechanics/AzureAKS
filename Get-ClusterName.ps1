31f5d9e3302d613caa92e1516941778161406810
Martin Palmer <martin.palmer@almmechanics.com>
2019-02-14 13:26:57 +0000

    modified name and id constructs

--- Get-ClusterName.ps1
+++ Get-ClusterName.ps1
@@ -5,4 +5,4 @@ param(
     $Id
 )
 
-write-host ("##vso[task.setvariable variable=Env.ClusterName]{0}" -f ("{0}aks{1:000}" -f $Prefix, $Id))
+write-host ("##vso[task.setvariable variable=ClusterName]{0}" -f ("{0}aks{1:000}" -f $Prefix, $Id))
--- Get-OmsId.ps1
+++ Get-OmsId.ps1
@@ -8,4 +8,4 @@ param(
 $OMSName = ("{0}-oms-{1:000}" -f $Prefix, $Id)
 $OMS = @(Get-AzureRmOperationalInsightsWorkspace) | select-object -first 1
 
-write-host ("##vso[task.setvariable variable=Env.OmsWorkspaceId]{0}" -f ($OMS.ResourceId))
+write-host ("##vso[task.setvariable variable=OmsWorkspaceId]{0}" -f ($OMS.ResourceId))
