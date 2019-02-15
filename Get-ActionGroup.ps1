7cea482846f18ed0eb6107007503a202e165cdd0
Martin Palmer <martin.palmer@almmechanics.com>
2019-02-15 08:23:52 +0000

    changed from name to resource id

--- Get-ActionGroup.ps1
+++ Get-ActionGroup.ps1
@@ -5,4 +5,6 @@ param(
     $Id
 )
 
-write-host ("##vso[task.setvariable variable=ActionGroup]{0}" -f ("{0}-ag-email-{1:000}" -f $Prefix, $Id))
+$actionGroupName = ("{0}-ag-email-{1:000}" -f $Prefix, $Id)
+$agResourceId = (Get-AzureRmResource -ResourceType  'Microsoft.Insights/actiongroups' -Name $actionGroupName).ResourceId
+write-host ("##vso[task.setvariable variable=ActionGroup]{0}" -f $agResourceId)
